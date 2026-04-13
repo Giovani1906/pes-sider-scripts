31d79aca31224ffa05f1c6ab5a19c39b612e33fd
Giovani1906 <4cc.giovani1906@gmail.com>
2026-04-13 19:14:59 +0200

    Make the script not swap the boot when a team is on their first kit

--- bootkitswap.lua
+++ bootkitswap.lua
@@ -2,18 +2,22 @@ function boot_kit_swap(ctx, filename)
 	if ctx.home_team or ctx.away_team then
 		local boot_id = tonumber((string.match(filename, "Asset\\model\\character\\boots\\k(%d+)\\#Win\\boots.fpk")))
 		if boot_id then
-			if 100 > boot_id or boot_id > 9900 then
-				return
-			end
+			if boot_id > 100 and boot_id < 9900 then
+				local home_kit = ctx.kits.get_current_kit_id(0) + 1
+				if home_kit ~= 1 then
+					local home_boot = ((ctx.home_team - 700) * 25) + 76
+					if boot_id >= home_boot and boot_id <= (home_boot + 24) then
+						return string.gsub(filename, "\\#Win\\", string.format("p%s\\#Win\\", home_kit))
+					end
+				end
 
-			local home_range = ((ctx.home_team - 700) * 25) + 76
-			if boot_id >= home_range and boot_id <= (home_range + 24) then
-				return string.gsub(filename, "\\#Win\\", string.format("p%s\\#Win\\", (ctx.kits.get_current_kit_id(0) + 1)))
-			end
-
-			local away_range = ((ctx.away_team - 700) * 25) + 76
-			if boot_id >= away_range and boot_id <= (away_range + 24) then
-				return string.gsub(filename, "\\#Win\\", string.format("p%s\\#Win\\", (ctx.kits.get_current_kit_id(1) + 1)))
+				local away_kit = ctx.kits.get_current_kit_id(1) + 1
+				if away_kit ~= 1 then
+					local away_boot = ((ctx.away_team - 700) * 25) + 76
+					if boot_id >= away_boot and boot_id <= (away_boot + 24) then
+						return string.gsub(filename, "\\#Win\\", string.format("p%s\\#Win\\", away_kit))
+					end
+				end
 			end
 		end
 	end
